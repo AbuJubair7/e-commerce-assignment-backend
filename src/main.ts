acc66847f1b16a98b4c2273d582e39a02f8c64b2
AbuJubair7 <jubaircode7@gmail.com>
2026-07-01 09:49:49 +0600

    chore(swagger): modified the url for access the doc

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
